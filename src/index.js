79008592d33a5cec2335621184d34e50a5c77f78
Christian <cvernikoff@gmail.com>
2018-02-25 15:17:02 -0800

    json-lock

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ import App from './App';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './rootReducer';
 import {userLoggedIn} from "./actions/auth";
 
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(thunk)),
+    applyMiddleware(thunk),
 );
 
 if (localStorage.user) {
